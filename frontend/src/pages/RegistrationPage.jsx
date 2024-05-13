077f2084f4c09e23ebebe436af322e475cdf2dcf
menachen shuvax <menachemshuvax@gmail.com>
2024-05-13 18:38:34 +0300

    signUpFunctions

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+
 const RegistrationPage = () => {
+  
+
   const [accountType, setAccountType] = useState('');
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
