250828045ba6851645e3fe82802547388557e4ca
menachen shuvax <menachemshuvax@gmail.com>
2024-05-15 16:18:45 +0300

    change phone

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -24,11 +24,7 @@ const RegistrationPage = () => {
         password,
         firstName,
         lastName,
-<<<<<<< HEAD
         phoneNumber,
-=======
-        phoneNumber
->>>>>>> 33942f66245ac17015d5d162ecd72fd83b308715
       });
 
       if (response.data) {
