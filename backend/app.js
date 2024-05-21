d538a1a377cdbfa54837b10ff1b64864f2680900
MeirNeustadt <msn.binah.1@gmail.com>
2024-05-21 13:15:08 +0300

    21/5 13:00

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const userRouter = require('./routes/userRoutes')
-const classRouter = require('./routes/classRoutes')
+//const classRouter = require('./routes/classRoutes')
 
 const app = express()
 const cors = require('cors')
@@ -11,10 +11,10 @@ const AppError = require('./utils/AppError')
 app.use(cors())
 app.use(express.json())
 app.use('/api/users', userRouter )
-app.use('/api/classes', classRouter)
+//app.use('/api/classes', classRouter)
  
 app.all('*', (req, res, next)=>{
-  return next(new AppError(404, 'This route is not exist'))
+  return next(new AppError (404, 'This route is not exist'))
 })
 app.use(globalErrorHandler)
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,33 +7,29 @@ router.route('/register')
     .post(userControllers.createUser)
 
 
-router.route('/class')
-    .get(userControllers.renderStudentClass)
-    .post(userControllers.createUser)
+// router.route('/class')
+//     .get(userControllers.renderStudentClass)
+//     .post(userControllers.createUser)
 
-router.route('/login')  
-<<<<<<< HEAD
-   // .post(userControllers.login)
-   .post((req, res)=>{
-    res.send()
-   })
-=======
-     .post(userControllers.login)
->>>>>>> 2695674f6aa6c52ef498323db8f6a5e6e00443d8
+// router.route('/login')  
+//    // .post(userControllers.login)
+//    .post((req, res)=>{
+//     res.send()
+//    })
 
 
-router.route('/class/file')
-     .post(userControllers.createFile)
-     .delete(userControllers.deleteFile)
+// router.route('/class/file')
+//      .post(userControllers.createFile)
+//      .delete(userControllers.deleteFile)
 
- router.route('/class/lesson')
-     .post(userControllers.createLesson)
-     .delete(userControllers.deleteLesson)
+//  router.route('/class/lesson')
+//      .post(userControllers.createLesson)
+//      .delete(userControllers.deleteLesson)
 
- router.route('/:email')
-     .get(userControllers.getUser)
-     .put(userControllers.updateUser)
-     .delete(userControllers.deleteUser)    
+//  router.route('/:email')
+//      .get(userControllers.getUser)
+//      .put(userControllers.updateUser)
+//      .delete(userControllers.deleteUser)    
 
 
 
