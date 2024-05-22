62d6dbcb958d014a427029ff0455129ffd8b216c
Chaim-Rosentaler <r0527135949@gmail.com>
2024-05-22 09:57:35 +0300

    09:57 22.05

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,37 +4,6 @@ const userControllers = require('./../controllers/userControllers')
 const router = express.Router()
 
 router.route('/register')
-    .post(userControllers.createUser)
-
-
-// router.route('/class')
-//     .get(userControllers.renderStudentClass)
-//     .post(userControllers.createUser)
-
-// router.route('/login')  
-//    // .post(userControllers.login)
-//    .post((req, res)=>{
-//     res.send()
-//    })
-
-
-// router.route('/class/file')
-//      .post(userControllers.createFile)
-//      .delete(userControllers.deleteFile)
-
-
- router.route('/class/lesson')
-     .post(userControllers.createLesson)
-     .delete(userControllers.deleteLesson)
-
-     
- router.route('/:email')
-     .get(userControllers.getUser)
-     .put(userControllers.updateUser)
-     .delete(userControllers.deleteUser)    
-
-
-
-
+    .post(authControllers.register)
 
 module.exports = router
