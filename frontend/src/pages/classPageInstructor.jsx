7bd60759cb508fc73833b1cb20b15ffcf66ee136
Israel Erlenger <y3966460@gmail.com>
2024-06-13 13:10:41 +0300

    13

--- frontend/src/pages/classPageInstructor.jsx
+++ frontend/src/pages/classPageInstructor.jsx
@@ -32,7 +32,6 @@ const ClassPageInstructor = () => {
     try {
       const response = await axios.get(`http://localhost:5000/api/instructorClass/${classId}`, { withCredentials: true });
       const { files, lessons, user, chats, liveLink, students } = response.data;
-      console.log('files:', files, 'lessons:', lessons, user, chats, 'link:', liveLink, 'students:', students);
       setFilesByCategory(files);
       setStudentsList(students);
       setLessons(lessons);
@@ -41,7 +40,6 @@ const ClassPageInstructor = () => {
       setLiveBroadcastLink(liveLink);
       setLoading(false);
     } catch (error) {
-      console.error(error);
       setError('Error fetching class data');
       setLoading(false);
     }
@@ -53,9 +51,7 @@ const ClassPageInstructor = () => {
 
     const intervalId = setInterval(() => {
       fetchClassData();
-    }, 600000000);
-    console.log('intervalId:', classId);
-
+    }, 600000);
     return () => clearInterval(intervalId);
   }, []);
 
@@ -79,7 +75,6 @@ const ClassPageInstructor = () => {
       setFilesByCategory([...filesByCategory, response.data.file]);
       setIsAddingFile(false);
     } catch (error) {
-      console.error(error);
       setError('Error adding file');
     }
   };
@@ -88,7 +83,6 @@ const ClassPageInstructor = () => {
     const { name, value } = event.target;
     if (name === 'newLessonName') setNewLessonName(value);
     if (name === 'newLessonDate') setNewLessonDate(value);
-    if (name === 'newLesson');
   };
 
   const handleAddLesson = async () => {
@@ -102,11 +96,18 @@ const ClassPageInstructor = () => {
       setLessons([...lessons, response.data.data]);
       setIsAddingLesson(false);
     } catch (error) {
-      console.error(error);
       setError('Error adding lesson');
     }
   };
-  
+
+  const handleDeleteLesson = async (lessonId) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/lessons/${lessonId}`, { withCredentials: true });
+      setLessons(lessons.filter(lesson => lesson._id !== lessonId));
+    } catch (error) {
+      setError('Error deleting lesson');
+    }
+  };
 
   const handleEditLiveBroadcastLink = async () => {
     try {
@@ -117,43 +118,12 @@ const ClassPageInstructor = () => {
       setLiveBroadcastLink(response.data.liveLink);
       setIsEditingBroadcast(false);
     } catch (error) {
-      console.error(error);
       setError('Error editing live broadcast link');
     }
   };
 
   const handleEditButtonClick = () => {
     setIsEditingBroadcast(true);
-  };
-
-  // const handleAddFile = async () => {
-  //   try {
-  //     await axios.post('http://localhost:5000/api/users/files/'), {
-  //       fileName,
-  //       fileDate,
-  //       fileCat
-  //     }
-  //   }
-  // }
-
-  const handleDeleteFile = async (fileId) => {
-    try {
-      await axios.delete(`http://localhost:5000/api/files/${fileId}`, { withCredentials: true });
-      setFilesByCategory(filesByCategory.filter(file => file.id !== fileId));
-    } catch (error) {
-      console.error(error);
-      setError('Error deleting file');
-    }
-  };
-
-  const handleDeleteLesson = async (streamId) => {
-    try {
-      await axios.delete(`http://localhost:5000/api/deleteLiveStream/${streamId}`, { withCredentials: true });
-      setLessons(lessons.filter(lesson => lesson.id !== streamId));
-    } catch (error) {
-      console.error(error);
-      setError('Error deleting live stream');
-    }
   };
 
   return (
@@ -196,7 +166,7 @@ const ClassPageInstructor = () => {
                             className="border border-gray-300 rounded px-3 py-2 w-full mb-2"
                           />
                           <input
-                            type="text"
+                            type="date"
                             placeholder="File date"
                             name="newFileDate"
                             value={newFileDate}
@@ -338,10 +308,18 @@ const ClassPageInstructor = () => {
                                 className="border border-gray-300 rounded px-3 py-2 w-full mb-2"
                               />
                               <input
-                                type="text"
-                                placeholder="Lesson Link"
+                                type="date"
+                                placeholder="Lesson Date"
                                 name="newLessonDate"
                                 value={newLessonDate}
+                                onChange={handleLessonInputChange}
+                                className="border border-gray-300 rounded px-3 py-2 w-full mb-2"
+                              />
+                              <input
+                                type="text"
+                                placeholder="Lesson Link"
+                                name="liveLink"
+                                value={liveBroadcastLink}
                                 onChange={handleLessonInputChange}
                                 className="border border-gray-300 rounded px-3 py-2 w-full mb-2"
                               />
@@ -390,7 +368,7 @@ const ClassPageInstructor = () => {
                       <div style={{ textAlign: 'center', flex: 1 }}>
                         <span className="text-gray-500">{new Date(lesson.date).toLocaleDateString('en-GB')}</span>
                       </div>
-                      <button onClick={() => handleDeleteLesson(lesson.id)}>
+                      <button onClick={() => handleDeleteLesson(lesson._id)}>
                         <FaTrash className="w-4 h-4 inline-block mx-1" style={{ verticalAlign: 'middle' }} />
                       </button>
                     </div>
