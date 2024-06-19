bc95caada9742063be81b4117ff91e304481724e
MeirNeustadt <msn.binah.1@gmail.com>
2024-06-19 17:41:06 +0300

    19/6 5:30

--- frontend/src/Context.jsx
+++ frontend/src/Context.jsx
@@ -1,14 +1,28 @@
 
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 
 const AppContext = createContext();
 
+const useLocalStorage = (key, initialValue) => {
+  // Retrieve value from localStorage
+  const storedValue = JSON.parse(localStorage.getItem(key));
+  const [value, setValue] = useState(storedValue || initialValue);
+
+  useEffect(() => {
+    // Store value in localStorage whenever it changes
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
+
 export const AppProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [classId, setClassId] = useState('665d7a1e65de6ed8c1b44b6c');
-  const [studentsList, setStudentsList] = useState([])
-  const [className, setClassName] = useState('Math4Algo')
-  const [chats, setChats] = useState([])
+  const [user, setUser] = useLocalStorage('user',null);
+  const [classId, setClassId] = useLocalStorage('classId','665d7a1e65de6ed8c1b44b6c');
+  const [studentsList, setStudentsList] = useLocalStorage('studentList',[])
+  const [className, setClassName] = useLocalStorage('className','Math4Algo')
+  const [chats, setChats] = useLocalStorage('chats',[])
   
 
   return (
